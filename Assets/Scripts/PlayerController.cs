fddd8fa5f9110aa2aa9a439c1f7bc47668a60034
Ryan Rudofsky <rrudofs0@email.cpcc.edu>
2021-02-26 16:33:09 -0500

    Adds playercontroller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed);
+        transform.Rotate(Vector3.forward);//back is right, forward is left
+    }
+}
